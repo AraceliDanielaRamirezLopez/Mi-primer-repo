ca090c5a62af27039790031f002fdad5e07f88ff
AraceliDanielaRamirezLopez <daniiela.ari33@gmail.com>
2020-10-19 16:03:59 -0500

    prueba 2
    se volvio a comprobar

--- Prueba.c
+++ Prueba.c
@@ -10,6 +10,6 @@ printf("Introduce radio (entero): ");
 scanf("%d", &r);
 l=2*PI*r; 
 a=PI*pow(r,2);
-printf("La longitud de la circunferencia vale %.3f\n", l);
-printf("El área del círculo vale %.3f\n", a);
+printf("La longitud de la circunferencia vale %.2f\n", l);
+printf("El área del círculo vale %.2f\n", a);
 }
